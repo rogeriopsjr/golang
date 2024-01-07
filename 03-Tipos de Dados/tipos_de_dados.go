96411fde6f010a9392b20bc6113d2fec24cbcd8a
Rogério PS Junior <rogeriopsjr@hotmail.com>
2024-01-07 15:45:49 -0300

    Tipos de Dados

---
+++ 03-Tipos de Dados/tipos_de_dados.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+// Tipos numéricos:  := -> quando por inferência é só int!!!
+// int8, int16, int32, int64
+// float32, float64
+	var idade int16 = 40
+	salario := 3000
+	altura := 1.77
+	fmt.Println(idade, altura, salario)
+}
